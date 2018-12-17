38ff12693ebda1366d77bddc2b593b47b83c21a8
Karel Molina <edmolina4@gmail.com>
2018-12-17 11:39:09 -0600

    User Story 2

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,8 +1,17 @@
+require_relative 'weather'
 class Airport
+  def initialize(weather)
+    @weather = weather
+  end
   def land plane
+
   end
 
   def take_off plane
-    
+    raise 'Weather is stormy. Planes can not land' if stormy?
+  end
+
+  def stormy?
+    @weather.stormy?
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,17 +1,26 @@
 require "airport"
 
 describe Airport do
+let (:airport){Airport.new}
+let (:plane) {Plane.new}
 
   describe '#Land' do
-    it "planes can land in airport" do
-      expect(subject).to respond_to(:land).with(1).argument
+    it "planes can land" do
+      expect(airport).to respond_to(:land).with(1).argument
     end
   end
 
   describe '#take_off' do
-    it 'plane take off airport'do
-      expect(subject).to respond_to(:take_off).with(1).argument
+    it 'plane take off'do
+      expect(airport).to respond_to(:take_off).with(1).argument
     end
+
+    context 'when is stormy' do
+      it 'planes can not land'do
+        expect{airport.take_off(plane)}.to raise_error 'Weather is stormy. Planes can not land'
+      end
+    end
+
 =begin
     it 'confirm plane leaves the airport' do
       expect to confirm planes leave that airport
