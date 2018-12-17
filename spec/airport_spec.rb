d39487e43b3a2f6cf66cafb3e8fe4544463535d0
Karel Molina <edmolina4@gmail.com>
2018-12-17 01:24:52 -0600

    User Story 2
    Instruct a plane to take off the airport

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,4 +4,8 @@ describe Airport do
   it "planes can land in airport" do
     expect(subject).to respond_to(:land).with(1).argument
   end
+
+  it 'plane take off airport'do
+    expect(subject).to respond_to(:take_off).with(1).argument
+  end
 end
